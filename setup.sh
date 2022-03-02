aa5636386d811d654bdb3e62ceb8fc554ab39f91
NaturalT314 <74961214+NaturalT314@users.noreply.github.com>
2022-03-02 16:06:04 +0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get install python3 python3-venv python3-pip
+sudo apt -y install python3-pip
+
+pip3 install pycryptodome
+pip3 install crypto
+pip3 install pwntools
