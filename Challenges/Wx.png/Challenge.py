cc28440cdb3657b70d053713277120a5f495b8ce
NaturalT314 <74961214+NaturalT314@users.noreply.github.com>
2022-03-06 14:40:34 +0300

    Update Challenge.py

--- Challenges/Wx.png/Challenge.py
+++ Challenges/Wx.png/Challenge.py
@@ -12,11 +12,12 @@ x = os.urandom(16)
 # xor the image with the 16 random bytes
 output = pwn.xor(image, x)
 
+# encode output as hex
+output = bytes.hex(output)
+
 # output result of xor into file named challenge.enc
-outputFile = open("cipher.enc", "wb").write(output)
-
-
-x = "Rmlyc3QgMTYgYnl0ZXMgb2YgYW55IHBuZyBmaWxlIGFyZSBjb25zdGFudA=="
+outputFile = open("cipher.enc", "w").write(output)
 
 # uncomment to reveal hint
+# x = "Rmlyc3QgMTYgYnl0ZXMgb2YgYW55IHBuZyBmaWxlIGFyZSBjb25zdGFudA=="
 # print(base64.b64decode(x))
